c0e88be0e9f54dd14031f4c199518e466979053c
plaredspear <plaredspear@gmail.com>
2018-02-07 23:14:09 +0900

    할 일 목록 컴포넌트 추가

---
+++ src/components/v2/TaskList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="task-list">
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskList',
+  data () {
+    return {
+      tasks: [
+        'todo-vuejs 글 남기기',
+        '집 청소',
+        'github repository 청소'
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
